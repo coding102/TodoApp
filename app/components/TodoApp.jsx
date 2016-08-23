1051e65aa364a0520fc1c1ce8d1a6a649469780d
Marck <mercercountynj@gmail.com>
2016-08-23 13:16:37 -0400

    hardcoded array render

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'walk the dog'
+                }, {
+                    id: 2,
+                    text: 'clean my room'
+                }, {
+                    id: 3,
+                    text: 'clean the yard'
+                }, {
+                    id: 4,
+                    text: 'need to exercise'
+                }
+            ]
+        };
+    },
+
     render: function () {
+        // es6 destructuring
+        // var {todos} = this.state;
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={this.state.todos} />
             </div>
         );
     }
