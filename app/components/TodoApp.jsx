a1d55b2b4cec88b545b532862eda10ca0d781b2e
Marck <mercercountynj@gmail.com>
2016-08-24 20:46:37 -0400

    added TodoSearch component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
     getInitialState: function() {
         return {
+            showCompleted: false,
+            searchText: '',
             todos: [
                 {
                     id: 1,
@@ -27,12 +30,20 @@ var TodoApp = React.createClass({
         alert('new todo: ' + text);
     },
 
+    handleSearch: function (showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    },
+
     render: function () {
         // es6 destructuring
         // var {todos} = this.state;
 
         return (
             <div>
+                <TodoSearch onSearch={this.handleSearch} />
                 <TodoList todos={this.state.todos} />
                 <AddTodo onAddTodo={this.handleAddTodo} />
             </div>
