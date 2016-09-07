2eeaf746954510344dfd73a57d249b195c032f93
Marck <mercercountynj@gmail.com>
2016-09-07 16:35:45 -0400

    todo styling

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,6 +5,12 @@ var TodoList = React.createClass({
     render: function() {
         var {todos} = this.props;
         var renderTodos = () => {
+            if (todos.length === 0) {
+                return (
+                    <p className="container__message">Nothing To Do</p>
+                );
+            }
+
             return todos.map((todo) => {
                 return (
                     // ... spread out props to Todo component
