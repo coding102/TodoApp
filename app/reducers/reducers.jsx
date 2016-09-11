ae49a9e9ad07798de4a1f14ffe4af3aff9a92d2a
Marck <mercercountynj@gmail.com>
2016-09-10 21:47:47 -0400

    showCompleted reducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -6,3 +6,13 @@ export var searchTextReducer = (state = '', action) => {
             return state;
     };
 };
+
+
+export var showCompletedReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+        default: 
+        return state;
+    };
+};
